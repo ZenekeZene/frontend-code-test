6970d21f169a4db92e3b4ed58c416a96acbbfa08
¨hvillar¨ <hvillar@idealista.com>
2024-05-21 23:08:22 +0200

    feat: Box component using BoxText and BoxFolderAesthetic.

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { observer } from "mobx-react";
 import BoxDraggable from "../BoxDraggable/BoxDraggable";
 import { BoxTools } from "../BoxTools/BoxTools";
+import { BoxFolderAesthetic } from "../BoxFolderAesthetic/BoxFolderAesthetic";
+import { BoxText } from "../BoxText/BoxText";
 import "./Box.css";
 
 const Box = React.forwardRef((props, ref) => {
-  const textRef = React.useRef();
   const [isEditing, setIsEditing] = React.useState(false);
   const localRef = React.useRef();
+  const { box } = props;
 
   React.useImperativeHandle(ref, () => localRef.current);
 
@@ -20,51 +22,38 @@ const Box = React.forwardRef((props, ref) => {
     setIsEditing(!isEditing);
   };
 
-  const handleChangeText = () => {
-    props.box.setText(textRef.current.innerText);
+  const onTextBlur = (event) => {
+    box.setText(event.target.innerText);
   };
 
-  React.useEffect(() => {
-    if (isEditing) {
-      textRef.current.focus();
-    }
-  }, [isEditing]);
-
   return (
-    <BoxDraggable {...props} ref={localRef} onDoubleClick={handleDoubleClick}>
-      <div className="box__front"
-        style={{
-          minHeight: props.box.height,
-          backgroundColor: props.box.backgroundColor,
-          borderColor: props.box.darkerBackgroundColor,
-        }}
+    <BoxDraggable
+      {...props}
+      ref={localRef}
+      onDoubleClick={handleDoubleClick}
+    >
+      <BoxFolderAesthetic
+        height={props.height}
+        backgroundColor={box.backgroundColor}
+        darkerBackgroundColor={box.darkerBackgroundColor}
       >
         { !props.areMultipleBoxesSelected && (
           <BoxTools
-            box={props.box}
+            box={box}
             isEditing={isEditing}
             onEdit={handleEdit}
             onRemove={props.onRemove}
           />
         )}
-        <svg className="box__ribbon" viewBox="0 0 53 7" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin:'round', strokeMiterlimit: '2' }}>
-          <path style={{ fill: 'none' }} d="M0 0h53.535v7.705H0z"/>
-          <path d="M52.553 6.947V4.033a3 3 0 0 0-3-3h-42L3.31 5.276A6 6 0 0 1 .083 6.947h52.47Z" style={{ fill: props.box.backgroundColor }} />
-        </svg>
 
-        <div ref={textRef}
-          className="box__text"
-          contentEditable={isEditing}
-          onInput={handleChangeText}
-          style={{ color: props.box.color }}
-        >{props.box.text}</div>
-      </div>
-      <div className="box__paper"></div>
-      <div className="box__back"
-        style={{ backgroundColor: props.box.darkerBackgroundColor }}
-      >
-      </div>
-      <svg className="box__ribbon-back" viewBox="0 0 39 7" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: '2' }}><path d="m38.828 6.828-4.071-4.071A6 6 0 0 0 30.515 1H3a3 3 0 0 0-3 3v2.828h38.828Z" style={{ fill: props.box.darkerBackgroundColor }}/></svg>
+        <BoxText
+          isEditing={isEditing}
+          text={box.text}
+          color={box.color}
+          onBlur={onTextBlur}
+        />
+
+      </BoxFolderAesthetic>
     </BoxDraggable>
   );
 });
