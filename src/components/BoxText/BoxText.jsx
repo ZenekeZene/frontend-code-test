d5d72745b750a1daa444c1265f0346f0b8603e9b
¨hvillar¨ <hvillar@idealista.com>
2024-05-21 23:07:46 +0200

    feat: added BoxText component.

---
+++ src/components/BoxText/BoxText.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './BoxText.css';
+
+const BoxText = ({ isEditing, color, text, onBlur  }) => {
+	const textRef = React.useRef();
+
+  const focusInLastCharacter = React.useCallback(() => {
+    const range = document.createRange();
+    const sel = window.getSelection();
+    range.selectNodeContents(textRef.current);
+    range.collapse(false);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }, []);
+
+  React.useEffect(() => {
+    if (document.activeElement === textRef.current) return;
+    isEditing && focusInLastCharacter();
+  }, [isEditing, focusInLastCharacter]);
+
+	return (
+		<div ref={textRef}
+			className="box__text"
+			contentEditable={isEditing}
+			suppressContentEditableWarning
+			onBlur={onBlur}
+			style={{ color }}
+		>{text}</div>
+	);
+};
+
+export { BoxText };
