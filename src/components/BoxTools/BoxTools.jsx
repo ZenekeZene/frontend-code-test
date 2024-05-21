e9699fbe4730167fa30c1ec970708a324c33d1de
¨hvillar¨ <hvillar@idealista.com>
2024-05-21 23:05:41 +0200

    style: added file BoxTools.css. Renaming classnames (BEM)

--- src/components/BoxTools/BoxTools.jsx
+++ src/components/BoxTools/BoxTools.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IconDelete } from "../../icons/IconDelete";
 import { IconEdit } from "../../icons/IconEdit";
 import { IconFontColors } from "../../icons/IconFontColors";
+import "./BoxTools.css";
 
 const BoxTools = ({ box, isEditing, onEdit, onRemove }) => {
 	const { backgroundColor } = box;
@@ -15,21 +16,21 @@ const BoxTools = ({ box, isEditing, onEdit, onRemove }) => {
   };
 
 	return (
-		<section className="box__tools">
-			<span className="box__bgcolor box__tool">
-				<span className="box__bgcolor-watch" style={{ backgroundColor }}></span>
+		<section className="box-tools">
+			<span className="box-tools__bgcolor box-tools__tool">
+				<span className="box-tools__bgcolor-watch" style={{ backgroundColor }}></span>
 				<input type="color" onChange={handleChangeBackgroundColor} />
 			</span>
-			<span className="box__color box__tool">
+			<span className="box-tools__color box-tools__tool">
 				<IconFontColors />
 				<input type="color" onChange={handleChangeColor} />
 			</span>
-			<span className="box__remove box__tool"
+			<span className="box-tools__remove box-tools__tool"
 				onClick={onRemove}
 			>
 				<IconDelete />
 			</span>
-			<span className={`box__edit box__tool ${isEditing ? '--is-editing': ''}`}
+			<span className={`box-tools__edit box-tools__tool ${isEditing ? '--is-editing': ''}`}
 				onClick={onEdit}
 			>
 				<IconEdit />
