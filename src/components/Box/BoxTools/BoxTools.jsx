305aaf93ee994139eb3cffd32f68f188ecfc158a
¨hvillar¨ <hvillar@idealista.com>
2024-05-23 19:10:13 +0200

    test: added BoxTools spec.

--- src/components/Box/BoxTools/BoxTools.jsx
+++ src/components/Box/BoxTools/BoxTools.jsx
@@ -3,6 +3,8 @@ import { Icon } from "../../../icons";
 import "./BoxTools.css";
 
 const BoxTools = ({ box, onRemove }) => {
+	if (!box) return;
+
 	const handleChangeColor = (event) => {
 		box.changeCurrentColor(event.target.value);
   };
@@ -26,9 +28,10 @@ const BoxTools = ({ box, onRemove }) => {
   };
 
 	return (
-		<section className="box-tools">
+		<section className="box-tools" role="menu">
 			<span className="box-tools__bgcolor box-tools__tool"
 				onBlur={handleBlurBackgroundColor}
+				role="menuitem"
 			>
 				<span className="box-tools__bgcolor-watch"
 					style={{ backgroundColor: box.isEditingBackgroundColor ? box.currentBackgroundColor : box.backgroundColor }}
@@ -37,25 +40,34 @@ const BoxTools = ({ box, onRemove }) => {
 					onFocus={() => box.setIsEditingBackgroundColor(true)}
 					onChange={handleChangeBackgroundColor}
 					value={box.isEditingBackgroundColor ? box.currentBackgroundColor : box.backgroundColor}
+					role="input"
+					aria-label="change background color"
 				/>
 			</span>
 			<span className="box-tools__color box-tools__tool"
 				onBlur={handleBlurColor}
+				role="menuitem"
 			>
 				<Icon.FontColors />
 				<input type="color"
 					onFocus={() => box.setIsEditingColor(true)}
 					onChange={handleChangeColor}
 					value={box.isEditingColor ? box.currentColor : box.color}
+					aria-label="change text color"
+					role="input"
 				/>
 			</span>
 			<span className="box-tools__remove box-tools__tool"
 				onClick={onRemove}
+				role="menuitem"
+				aria-label="delete box"
 			>
 				<Icon.Delete />
 			</span>
 			<span className={`box-tools__edit box-tools__tool ${box.isEditingText ? '--is-editing': ''}`}
 				onClick={handleEditText}
+				role="menuitem"
+				aria-label="edit text"
 			>
 				<Icon.Edit />
 			</span>
---
+++ src/components/Box/BoxTools/BoxTools.spec.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BoxTools } from './BoxTools';
+
+const createBox = (props) => ({
+	changeCurrentBackgroundColor: vi.fn(),
+	changeCurrentColor: vi.fn(),
+	setIsEditingBackgroundColor: vi.fn(),
+	setIsEditingColor: vi.fn(),
+	currentBackgroundColor: '#ffffff',
+	backgroundColor: '#ffffff',
+	...props,
+});
+
+describe('BoxTools component:', () => {
+	test(`does not render unless a box is passed to it `, () => {
+		render(<BoxTools />);
+
+		expect(() => screen.getByRole('menu')).toThrow();
+	});
+
+	test(`renders the menu and his four options if a box is passed`, () => {
+		const box = createBox();
+		render(<BoxTools box={box} />);
+
+		expect(screen.getAllByRole('menuitem')).toHaveLength(4);
+	});
+
+	test(`the callback "onRemove" is called when the user
+		clicks on delete option`, async () => {
+		const box = createBox();
+		const onRemove = vi.fn();
+		render(<BoxTools box={box} onRemove={onRemove} />);
+
+		const deleteOption = screen.getByRole('menuitem', { name: /delete/i });
+
+		await userEvent.click(deleteOption);
+
+		expect(onRemove).toHaveBeenCalled();
+	});
+
+	test(`the callback "changeCurrentColor" of the box is called when the user
+		changes the color input`, () => {
+		const changeCurrentColor = vi.fn();
+		const box = createBox({ changeCurrentColor });
+		render(<BoxTools box={box} />);
+
+		const colorInput = screen.getByRole('input', { name: /change text color/i });
+
+		fireEvent.input(colorInput, { target: { value: '#000000' } });
+
+		expect(changeCurrentColor).toHaveBeenCalledWith('#000000');
+	});
+
+	test(`the callback "changeCurrentBackgroundColor" of the box is called when the user
+		changes the background color input`, () => {
+		const changeCurrentBackgroundColor = vi.fn();
+		const box = createBox({ changeCurrentBackgroundColor });
+		render(<BoxTools box={box} />);
+
+		const colorInput = screen.getByRole('input', { name: /change background color/i });
+
+		fireEvent.input(colorInput, { target: { value: '#000000' } });
+
+		expect(changeCurrentBackgroundColor).toHaveBeenCalledWith('#000000');
+	});
+
+	test(`the callback "changeColor" of the box is called when the user
+		leaves the color input`, () => {
+		const changeColor = vi.fn();
+		const box = createBox({ changeColor });
+		render(<BoxTools box={box} />);
+
+		const colorInput = screen.getByRole('input', { name: /change text color/i });
+
+		fireEvent.blur(colorInput, { target: { value: '#000000' } });
+
+		expect(changeColor).toHaveBeenCalledWith('#000000');
+	});
+
+	test(`the callback "changeBackgroundColor" of the box is called when the user
+		leaves the background color input`, () => {
+		const changeBackgroundColor = vi.fn();
+		const box = createBox({ changeBackgroundColor });
+		render(<BoxTools box={box} />);
+
+		const colorInput = screen.getByRole('input', { name: /change background color/i });
+
+		fireEvent.blur(colorInput, { target: { value: '#000000' } });
+
+		expect(changeBackgroundColor).toHaveBeenCalledWith('#000000');
+	});
+
+	test(`the callback "setIsEditingColor" of the box is called when the user
+		focus the color input`, () => {
+		const setIsEditingColor = vi.fn();
+		const box = createBox({ setIsEditingColor });
+		render(<BoxTools box={box} />);
+
+		const colorInput = screen.getByRole('input', { name: /change text color/i });
+
+		fireEvent.focus(colorInput);
+
+		expect(setIsEditingColor).toHaveBeenCalledWith(true);
+	});
+
+	test(`the callback "setIsEditingBackgroundColor" of the box is called when the user
+		focus the background color input`, () => {
+		const setIsEditingBackgroundColor = vi.fn();
+		const box = createBox({ setIsEditingBackgroundColor });
+		render(<BoxTools box={box} />);
+
+		const colorInput = screen.getByRole('input', { name: /change background color/i });
+
+		fireEvent.focus(colorInput);
+
+		expect(setIsEditingBackgroundColor).toHaveBeenCalledWith(true);
+	});
+
+	test(`the callback "setIsEditingText" of the box is called when the user
+		clicks on the text option`, async () => {
+		const setIsEditingText = vi.fn();
+		const box = createBox({ setIsEditingText });
+		render(<BoxTools box={box} />);
+
+		const textOption = screen.getByRole('menuitem', { name: /edit text/i });
+
+		await userEvent.click(textOption);
+
+		expect(setIsEditingText).toHaveBeenCalledWith(true);
+	});
+});
