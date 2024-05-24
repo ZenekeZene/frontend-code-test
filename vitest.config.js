ad3b5f2d88dba692ef457c7c02ea98030c213c24
¨hvillar¨ <hvillar@idealista.com>
2024-05-24 09:07:36 +0200

    tools: installing Playwright and its config

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,7 @@ export default {
     globals: true,
     environment: "jsdom",
     setupFiles: ["./setupTests.js"],
+    include: ["./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
 };
