719fa7c8c7586c7a572b79253174779b8712764b
¨hvillar¨ <hvillar@idealista.com>
2024-05-26 18:50:04 +0200

    feat: counter with number of selected boxes

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -6,15 +6,21 @@ import "./Counter.css";
 const Counter = observer(({ store }) => {
   const isAnyBoxSelected = store.isAnyBoxSelected();
   const numberOfBoxes = store.getNumberOfCreatedBoxes();
+  const numberOfSelectedBoxes = store.getNumberOfSelectedBoxes();
 
   return (
-    <ul className="counter" aria-label={`There are ${numberOfBoxes} boxes`}>
-      <CounterFeed
-        boxes={store.boxes}
-        isAnyBoxSelected={isAnyBoxSelected}
-        onClick={store.selectSingleBox}
-      />
-    </ul>
+    <section className="counter-wrapper">
+      <ul className="counter" aria-label={`There are ${numberOfBoxes} boxes`}>
+        <CounterFeed
+          boxes={store.boxes}
+          isAnyBoxSelected={isAnyBoxSelected}
+          onClick={store.selectSingleBox}
+        />
+      </ul>
+      { numberOfSelectedBoxes > 0 && (
+        <span className="counter__value">({numberOfSelectedBoxes})</span>
+      )}
+    </section>
   );
 });
 
